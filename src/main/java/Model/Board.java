5bf2f7ec1e477e5c00ea9da22443139db023102e
Samu Sebestyen Vaczi <samuseb@gmail.com>
2021-11-06 17:33:56 +0100

    Board and Task became jpa entities

--- src/main/java/Model/Board.java
+++ src/main/java/Model/Board.java
@@ -5,18 +5,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 
 @Getter
 @Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Board {
 
+    @Id
+    @GeneratedValue
     private long id;
+
     private String title;
+
+    @OneToMany(mappedBy = "board")
     private List<Task> tasks;
+
+    @OneToMany(mappedBy = "board")
     private List<Ad> ads;
+
+
+
 
     public Board(String title){ this.title = title; }
 
--- src/main/java/Model/Task.java
+++ src/main/java/Model/Task.java
@@ -1,18 +1,38 @@
 package Model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Getter
 @Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
 
+    @Id
+    @GeneratedValue
     private long id;
+
     private String name;
+
     private String description;
+
+    @ManyToOne
     private User assignedTo;
+
+    @ManyToOne
     private Category category;
+
+    @ManyToOne
     private Board board;
 
 
