c14f62f049bcb442770592e7d8ac79baf1b9babe
Samu Sebestyen Vaczi <samuseb@gmail.com>
2021-11-29 21:46:05 +0100

    Typo fixed

--- src/main/java/kanban/service/SampleDataService.java
+++ src/main/java/kanban/service/SampleDataService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class SapmleDataService {
+public class SampleDataService {
 
     @Autowired
     ProjectRepository projectRepository;
