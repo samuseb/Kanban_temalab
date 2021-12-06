74df9cabe0ae7eb4236c22f60ee580e8992a9740
Samu Sebestyen Vaczi <samuseb@gmail.com>
2021-12-06 21:14:59 +0100

    Navbar and usersPage added

---
+++ src/main/java/kanban/web/UserController.java
@@ -0,0 +1,38 @@
+package kanban.web;
+
+
+import kanban.model.User;
+import kanban.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Map;
+
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+
+
+    @GetMapping("/users")
+    public String home(Map<String, Object> model) {
+        List<User> users = userRepository.findAll();
+        model.put("users", users);
+        model.put("user", new User());
+
+        return "usersPage";
+    }
+
+    @PostMapping("/createUser")
+    public String createUser(User user) {
+        userRepository.save(user);
+        return "redirect:/users";
+    }
+}
