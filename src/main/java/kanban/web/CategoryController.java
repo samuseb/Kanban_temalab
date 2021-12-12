5f8f2bcc3b979884d3b257a963bf993ae1f85ebe
Samu Sebestyen Vaczi <samuseb@gmail.com>
2021-12-12 10:52:27 +0100

    CategoriesPage added

---
+++ src/main/java/kanban/web/CategoryController.java
@@ -0,0 +1,37 @@
+package kanban.web;
+
+import kanban.model.Category;
+import kanban.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Map;
+
+
+@Controller
+public class CategoryController {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+
+
+
+    @GetMapping("/categories")
+    public String home(Map<String, Object> model) {
+        List<Category> categories = categoryRepository.findAll();
+        model.put("categories", categories);
+        model.put("category", new Category());
+
+        return "categoriesPage";
+    }
+
+    @PostMapping("/createCategory")
+    public String createUser(Category category) {
+        categoryRepository.save(category);
+        return "redirect:/categories";
+    }
+}
